9d0eaed4dd9355ec3e2cf8b82bb96f7833af708b
Olga Blud <olga.blud@gmail.com>
2023-12-11 23:22:03 +0200

    added basic css

--- src/Weather.js
+++ src/Weather.js
@@ -10,10 +10,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -28,7 +33,8 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Weather icon"
           />
-          6℃
+          <span className="temperature">6</span>
+          <span className="unit">℃</span>
           {/* <p> 6℃</p> */}
         </div>
         <div className="col-6">
