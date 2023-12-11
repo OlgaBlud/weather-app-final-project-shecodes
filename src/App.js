757b37a31da00076d8874c99d953d7da043b61ac
Olga Blud <olga.blud@gmail.com>
2023-12-11 21:31:50 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Olga Blud and is{" "}
-        <a
-          href="https://github.com/OlgaBlud/weather-app-final-project-shecodes"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Olga Blud and is{" "}
+          <a
+            href="https://github.com/OlgaBlud/weather-app-final-project-shecodes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
