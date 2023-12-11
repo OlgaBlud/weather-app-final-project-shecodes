17ccac3a391cd9dccc5c709431e640c57ed98e5f
Olga Blud <olga.blud@gmail.com>
2023-12-11 18:29:53 +0200

    added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Olga Blud and is{" "}
+        <a
+          href="https://github.com/OlgaBlud/weather-app-final-project-shecodes"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
