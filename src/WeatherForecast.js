559e5e378f7132fb34c068ced654e71611cd3278
Olga Blud <olga.blud@gmail.com>
2023-12-18 21:05:30 +0200

    get real temperature and day  for the forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,33 +1,31 @@
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
+import { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  //   const apiKey = "dfe55254099b96bf9592c69aca42d6f5";
-  const apiKey = "1a87a8te48d8o34feb04cce044538ce4";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
 
-  //   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-  const apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
-  console.log(apiUrl);
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
+    // console.log(response.data.daily);
     // console.log(props);
   }
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="weather-forecast">
-      <div className="row">
-        <div className="col">
-          <div className="forecast-day">Tue</div>
-          <WeatherIcon iconCode="01d" width="30%" />
-          <div className="forecast-min-max-temperatures">
-            <span className="forecast-max-temp">15</span>
-            <span className="forecast-min-temp">-5</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  if (loaded) {
+    console.log(forecast[0]);
+    return <WeatherForecastDay data={forecast[0]} />;
+  } else {
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    const apiKey = "1a87a8te48d8o34feb04cce044538ce4";
+    const apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+    //   const apiKey = "dfe55254099b96bf9592c69aca42d6f5";
+    //   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    //   console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+
+    // return null;
+  }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,26 @@
+import WeatherIcon from "./WeatherIcon";
+export default function WeatherForecastDay(props) {
+  let maxTemperature = Math.round(props.data.temperature.maximum);
+  let minTemperature = Math.round(props.data.temperature.minimum);
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    let day = date.getDay();
+    return days[day];
+  }
+
+  return (
+    <div className="weather-forecast">
+      <div className="row">
+        <div className="col">
+          <div className="forecast-day">{day()}</div>
+          <WeatherIcon iconCode="01d" width="30%" />
+          <div className="forecast-min-max-temperatures">
+            <span className="forecast-max-temp">{maxTemperature}ºC</span>
+            <span className="forecast-min-temp">{minTemperature}ºC</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
